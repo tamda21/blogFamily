d1fdc6cab27904120ec1ecd9db6e41a23e2d8a01
tamda21 <abdetam452@gmail.com>
2022-11-09 21:45:34 +0100

    checkbox in vue

--- src/components/addBlog.vue
+++ src/components/addBlog.vue
@@ -6,16 +6,36 @@
     <input type="text" class="form-control"  v-model.lazy="blog.title">
   </div>
   <div class="mb-3">
-    <label  class="form-label">Blog Content</label>
-    <input type="text" class="form-control"  v-model.lazy="blog.content">
+    <label  class="form-label d-inline-block">Blog Content</label>
+    <input type="text" class="form-control d-inline-block"  v-model.lazy="blog.content">
+  </div>
+  <div class="form-check">
+  <label class="form-check-label d-inline-block" >
+      Abderrahim
+  </label>
+  <input class="form-check-input d-inline-block" type="checkbox" value="Abderrahim" v-model="blog.familly">
+  </div>
+  <div class="form-check">
+  <label class="form-check-label d-inline-block" >
+      Meryem
+  </label>
+  <input class="form-check-input d-inline-block" type="checkbox" value="Meryem" v-model="blog.familly">
+  </div>
+  <div class="form-check">
+  <label class="form-check-label d-inline-block"  >
+      Loujaine
+  </label>
+  <input class="form-check-input d-inline-block" type="checkbox" value="Loujaine" v-model="blog.familly">
   </div>
 </form>
     <div class="card" style="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title">Blog title :{{blog.title}}</h5>
-    <p class="card-text">Blog Contact </p>
+      <div class="card-body">
+    <h5 class="card-title">Blog title  : {{blog.title}}</h5>
+    <p class="card-text">Blog Contact :</p>
     <p class="card-text">{{blog.content}}</p>
-    
+    <ul class="list-group">
+  <li class="list-group-item" v-for="family in blog.familly">{{family}}</li>
+    </ul>
   </div>
 </div>
   </div>
@@ -28,7 +48,8 @@ export default {
     return {
       blog:{
         title:"",
-        content:""
+        content:"",
+        familly:[]
       }
     }
   }

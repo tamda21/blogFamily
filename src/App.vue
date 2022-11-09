32742e7de3af311c78980cdfdc5b80a9a91913f3
tamda21 <abdetam452@gmail.com>
2022-11-09 21:23:13 +0100

    Start Blog Familly

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <add-blog></add-blog>
+  </div>
+</template>
+
+<script>
+import addBlog from './components/addBlog'
+export default {
+  components:{
+    'add-blog':addBlog
+  },
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style>
+
+
+</style>
---
+++ src/components/addBlog.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="add-blog">
+    <form>
+  <div class="mb-3">
+    <label class="form-label">Blog Title</label>
+    <input type="text" class="form-control"  v-model.lazy="blog.title">
+  </div>
+  <div class="mb-3">
+    <label  class="form-label">Blog Content</label>
+    <input type="text" class="form-control"  v-model.lazy="blog.content">
+  </div>
+</form>
+    <div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">Blog title :{{blog.title}}</h5>
+    <p class="card-text">Blog Contact </p>
+    <p class="card-text">{{blog.content}}</p>
+    
+  </div>
+</div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+  data () {
+    return {
+      blog:{
+        title:"",
+        content:""
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+
+</style>

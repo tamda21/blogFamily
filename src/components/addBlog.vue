35c48f6ffde8ca3f7f5f447844fabe072f543464
tamda21 <abdetam452@gmail.com>
2022-11-10 23:34:58 +0100

    select box in vuejs

--- src/components/addBlog.vue
+++ src/components/addBlog.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="add-blog">
+    <h1 class="text-center text-bg-danger p-3">Blog Family</h1>
     <form>
   <div class="mb-3">
     <label class="form-label">Blog Title</label>
@@ -27,6 +28,10 @@
   </label>
   <input class="form-check-input d-inline-block" type="checkbox" value="Loujaine" v-model="blog.familly">
   </div>
+  <select class="form-select m-3" v-model="blog.fruit">
+  <option v-for="fruit in fruits" > {{fruit}}</option>
+</select>
+<div class="btn-primary" v-on:click.prevent="post">Add Blog</div>
 </form>
     <div class="card" style="width: 18rem;">
       <div class="card-body">
@@ -36,6 +41,7 @@
     <ul class="list-group">
   <li class="list-group-item" v-for="family in blog.familly">{{family}}</li>
     </ul>
+    <p>Fever Food : {{blog.fruit}}</p>
   </div>
 </div>
   </div>
@@ -49,8 +55,19 @@ export default {
       blog:{
         title:"",
         content:"",
-        familly:[]
-      }
+        familly:[],
+        fruit:""
+      },
+      fruits:['banana','strawberry','orange', 'apple']
+    }
+  },
+  methods:{
+    post:function(){
+      this.$http.post('',{
+        title:blog.title,
+        body:blog.content,
+        userId:1
+      })
     }
   }
 }

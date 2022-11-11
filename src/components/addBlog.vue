e6c4a55485971d47894143ad6b0cf067d0b1a02f
tamda21 <abdetam452@gmail.com>
2022-11-11 22:37:23 +0100

    Http request in vue get and set

--- src/components/addBlog.vue
+++ src/components/addBlog.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="add-blog">
     <h1 class="text-center text-bg-danger p-3">Blog Family</h1>
-    <form>
+    <form v-if="!submitted">
   <div class="mb-3">
     <label class="form-label">Blog Title</label>
     <input type="text" class="form-control"  v-model.lazy="blog.title">
@@ -31,8 +31,11 @@
   <select class="form-select m-3" v-model="blog.fruit">
   <option v-for="fruit in fruits" > {{fruit}}</option>
 </select>
-<div class="btn-primary" v-on:click.prevent="post">Add Blog</div>
+<button class="btn btn-primary" v-on:click.prevent="post">Add Blog</button>
 </form>
+<div class="alert alert-primary" role="alert" v-if="submitted">
+  Your Data Is Successfully Submitted 
+</div>
     <div class="card" style="width: 18rem;">
       <div class="card-body">
     <h5 class="card-title">Blog title  : {{blog.title}}</h5>
@@ -58,20 +61,24 @@ export default {
         familly:[],
         fruit:""
       },
-      fruits:['banana','strawberry','orange', 'apple']
+      fruits:['banana','strawberry','orange', 'apple'], 
+      submitted:false
     }
   },
   methods:{
     post:function(){
-      this.$http.post('',{
-        title:blog.title,
-        body:blog.content,
+      this.$http.post('https://jsonplaceholder.typicode.com/posts',{
+        title:this.blog.title,
+        body:this.blog.content,
         userId:1
+      }).then((data)=>{
+        console.log(data);
+        this.submitted=true
       })
     }
   }
 }
-</script>
+</script>  
 
 <style>
 
---
+++ src/components/showBlog.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <div class="card mb-4" v-for="blog in blogs">
+    <div class="card-body">
+    <h3 class="card-title">{{blog.title}}</h3>
+      <p class="card-text">{{blog.body}}</p>
+    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      blogs:[]
+    }
+  },
+  created(){
+  this.$http.get('https://jsonplaceholder.typicode.com/posts').then((data)=>{
+      this.blogs=data.body.slice(0,10);
+    })
+  }
+}
+</script>
+
+<style>
+
+
+</style>

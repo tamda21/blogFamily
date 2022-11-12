9c9af301f7c22a20f51f66ff2b4165ac5b7ebc81
tamda21 <abdetam452@gmail.com>
2022-11-12 08:14:14 +0100

    add custom search

--- src/components/showBlog.vue
+++ src/components/showBlog.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="container" v-media:bg="'sm'">
-    <div class="card mb-4" v-for="blog in blogs">
+    <h1>All Blogs</h1>
+ <div class="mb-3">
+    <label class="form-label">Search Blog</label>
+    <input type="text" class="form-control"  v-model="search" placeholder="search blog">
+  </div>
+    <div class="card mb-4" v-for="blog in filteredBlogs">
     <div class="card-body">
     <h3 class="card-title" v-rainbow>{{blog.title | upper-case }}</h3>
       <p class="card-text">{{blog.body | snippet}}</p>
@@ -14,13 +19,21 @@
 export default {
   data () {
     return {
-      blogs:[]
+      blogs:[],
+      search:''
     }
   },
   created(){
   this.$http.get('https://jsonplaceholder.typicode.com/posts').then((data)=>{
       this.blogs=data.body.slice(0,10);
     })
+  },
+  computed:{
+    filteredBlogs:function(){
+      return this.blogs.filter((blog)=>{
+        return blog.title.match(this.search)
+      })
+    }
   }
 }
 </script>
